1bc04ecf06ed41b29df2b2de0973c07a1b272d3f
Hoang Duong Trong <hd@aaaa.dyn.berkeley.edu>
2016-02-26 15:00:33 -0800

    Move the logging function from linear_model.py to utils.py

--- linear_models.py
+++ linear_models.py
@@ -4,31 +4,7 @@ from sklearn.metrics import r2_score, accuracy_score
 from sklearn.preprocessing import LabelBinarizer
 from tensorflow.examples.tutorials.mnist import input_data
 from preprocessing import SplitDataBatch
-
-try:
-    logger
-except NameError:
-    logging.basicConfig(format="[%(asctime)s] %(levelname)s\t%(message)s",
-            filename="history.log", 
-            filemode='a', level=logging.DEBUG,
-            datefmt='%m/%d/%y %H:%M:%S')
-    formatter = logging.Formatter("[%(asctime)s] %(levelname)s\t%(message)s",
-            datefmt='%m/%d/%y %H:%M:%S')
-    console = logging.StreamHandler()
-    console.setFormatter(formatter)
-    console.setLevel(logging.INFO)
-    logging.getLogger().addHandler(console)
-    logger = logging.getLogger(__name__)
-
-def PrintMessage(epoch = -1, train_loss = 0, valid_loss = 0, valid_score = 0,
-        norm_weight = 0):
-    if epoch == -1:
-        logger.info("%6s | %10s | %10s | %10s | %10s", "Epoch", "TrainLoss",
-                "ValidLoss", "ValidScore", "NormWeight")
-    else:
-        logger.info("%6d | %10.4g | %10.4g | %10.4g | %10.4g", epoch, train_loss, 
-                valid_loss, valid_score, norm_weight)
-
+from utils import *
 
 class LinearRegression(BaseEstimator):
     def __init__(self):
@@ -86,6 +62,13 @@ class LogisticRegression(BaseEstimator):
         self.verbose = verbose
         
     def fit(self, X = None, Y = None, data_function = None):
+        """ We train the model given the data. If the X and Y are provided, we
+        will create a data_function that split the data into three portions,
+        train, valid, and test. We need to do this because we want to feed in at
+        each iteration of Stochastic Gradient Descent only a batch of data.
+        data_function has a method next_batch(128) that will return the next 128
+        observations for training SGD
+        """
         if self.verbose: PrintMessage()
         np.random.seed(self.seed)
         if data_function is None:
---
+++ utils.py
@@ -0,0 +1,24 @@
+import logging
+try:
+    logger
+except NameError:
+    logging.basicConfig(format="[%(asctime)s] %(levelname)s\t%(message)s",
+            filename="/tmp/history.log",
+            filemode='a', level=logging.DEBUG,
+            datefmt='%m/%d/%y %H:%M:%S')
+    formatter = logging.Formatter("[%(asctime)s] %(levelname)s\t%(message)s",
+            datefmt='%m/%d/%y %H:%M:%S')
+    console = logging.StreamHandler()
+    console.setFormatter(formatter)
+    console.setLevel(logging.INFO)
+    logging.getLogger().addHandler(console)
+    logger = logging.getLogger(__name__)
+
+def PrintMessage(epoch = -1, train_loss = 0, valid_loss = 0, valid_score = 0,
+        norm_weight = 0):
+    if epoch == -1:
+        logger.info("%6s|%10s|%10s|%10s|%10s", "Epoch", "TrainLoss",
+                "ValidLoss", "ValidScore", "NormWeight")
+    else:
+        logger.info("%6d|%10.4g|%10.4g|%10.4g|%10.4g", epoch, train_loss,
+                valid_loss, valid_score, norm_weight)
