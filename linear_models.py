b804d21cfc31078c6c87f78abe5ea63962df8996
Hoang Duong Trong <hd@airbears2-10-142-9-82.airbears2.1918.berkeley.edu>
2016-02-25 22:12:02 -0800

    Add file process_data to get batch

--- linear_models.py
+++ linear_models.py
@@ -1,8 +1,8 @@
 import ipdb, tensorflow as tf, numpy as np
 from sklearn.base import BaseEstimator
 from sklearn.metrics import r2_score, accuracy_score
-import ipdb
-
+from tensorflow.examples.tutorials.mnist import input_data
+from process_data import *
 
 class LinearRegression(BaseEstimator):
     def __init__(self):
@@ -51,15 +51,23 @@ class RidgeRegression(BaseEstimator):
         return r2_score(ytest, self.predict(Xtest))
 
 class LogisticRegression(BaseEstimator):
-    def __init__(self, n_epoch = 2, batch_size = 128, ridge = 0., verbose = 0):
+    def __init__(self, n_epoch = 10, batch_size = 128, l1_penalty = 0., 
+            l2_penalty = 0., verbose = 0):
         self.n_epoch = n_epoch
         self.batch_size = batch_size
-        self.ridge = ridge
+        self.l2_penalty = l2_penalty
         self.verbose = verbose
 
-    def fit(self, X, Y):
+    def fit(self, X = None, Y = None, data_function = None):
+        if X is None:
+            X, Y = data_function.train.next_batch(self.batch_size)
+            n = data_function.train.num_examples
+        else: n = X.shape[0]
+        assert len(X.shape) == 2
+        assert len(X) == len(Y)
         np.random.seed(1)
-        n, p = X.shape
+        p = X.shape[1]
+        
         if len(Y.shape) == 1:
             YY = np.zeros((n,2))
             YY[:,0] = 1 - Y
@@ -74,8 +82,8 @@ class LogisticRegression(BaseEstimator):
         p = tf.nn.softmax(tf.matmul(x, W) + b)
         y = tf.placeholder(tf.float32, [None, q])
 
-        cross_entropy = -tf.reduce_sum(y * tf.log(p)) / n + \
-                self.ridge * tf.reduce_sum(W**2)
+        cross_entropy = -tf.reduce_sum(y * tf.log(p)) / self.batch_size + \
+                self.l2_penalty * tf.reduce_sum(W**2)
         train_step = tf.train.AdamOptimizer().\
                         minimize(cross_entropy)
         init = tf.initialize_all_variables()
@@ -83,12 +91,21 @@ class LogisticRegression(BaseEstimator):
         sess.run(init)
         n_iter = self.n_epoch * (n / self.batch_size)
         for i in xrange(n_iter):
-            sample = np.random.randint(n, size = self.batch_size)
-            sess.run(train_step, feed_dict = {x: X[sample], 
+            if data_function is None:
+                sample = np.random.randint(n, size = self.batch_size)
+                sess.run(train_step, feed_dict = {x: X[sample], 
                                               y: Y[sample]})
-            if self.verbose and (i % (n / self.batch_size) == 0):
-                print(sess.run(cross_entropy, feed_dict = {x: X[sample],
+                if self.verbose and (i % (n / self.batch_size) == 0):
+                    print(sess.run(cross_entropy, feed_dict = {x: X[sample],
                                                        y: Y[sample]}))
+            else:
+                batch_xs, batch_ys = data_function.train.next_batch(
+                        self.batch_size)
+                sess.run(train_step, feed_dict={x: batch_xs, y: batch_ys})
+                if self.verbose and (i % (n / self.batch_size) == 0):
+                    print(sess.run(cross_entropy, feed_dict = 
+                        {x: data_function.validation.images, 
+                         y: data_function.validation.labels}))
         self.W = sess.run(W)
         self.b = sess.run(b)
 
@@ -102,7 +119,7 @@ class LogisticRegression(BaseEstimator):
         return accuracy_score(ytest, yhat)
 
 
-if __name__ == '__main__':
+def TestOnSimulateData():
     n = 10000; p = 10
     X = np.random.randn(n,p).astype("float32")
     beta = np.random.randn(p).astype("float32")
@@ -110,14 +127,20 @@ if __name__ == '__main__':
     Y = np.zeros(n, dtype = "int")
     Y[y > mean(y)] = 1
 
-    if 0:
+    if 1:
         clf = LinearRegression()
         clf.fit(X, y)
         print clf.score(X, y)
-    if 0:
+    if 1:
         clf = RidgeRegression(alpha = 0.1)
         clf.fit(X, y)
         print clf.score(X, y)
+    if 1:
+        clf = LogisticRegression(n_epoch = 100, l2_penalty = 0.0001)
+        clf.fit(X, Y)
+        print clf.score(X, Y)
 
-    clf = LogisticRegression(n_epoch = 100, ridge = 0.0001)
-    clf.fit(X, Y)
+if __name__ == '__main__':
+    mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+    clf = LogisticRegression(n_epoch = 10, verbose = 1)
+    clf.fit(data_function = mnist)
---
+++ process_data.py
@@ -0,0 +1,34 @@
+class ProcessBatch():
+    def __init__(self, X, Y):
+        self.index = 0
+        self.images = X
+        self.labels = Y
+        self.num_examples = len(X)
+        self.epochs_completed = 0
+        assert len(X) == len(Y)
+    def next_batch(self, batch_size):
+        if self.index + batch_size < self.num_examples:
+            batch_index = np.arange(self.index, self.index + batch_size,
+                    dtype = int)
+        else:
+            batch_index = np.concatenate([
+                    np.arange(self.index, self.num_examples), 
+                    np.arange(0, batch_size - self.num_examples + self.index)])
+            self.epochs_completed += 1
+        self.index = (self.index + batch_size) % self.num_examples
+        return self.images[batch_index], self.labels[batch_index]
+
+class SplitDataBatch():
+    def __init__(self, X, Y, valid_ratio = .1, test_ratio = .1):
+        assert len(X) == len(Y)
+        n = len(X)
+        n1 = int(n * (1 - valid_ratio - test_ratio))
+        n2 = int(n * (1 - test_ratio))
+        self.train = ProcessBatch(X[:n1], Y[:n1])
+        self.valid = ProcessBatch(X[n1:n2], Y[n1:n2])
+        self.test  = ProcessBatch(X[n2:], Y[n2:])
+
+if __name__ == '__main__':
+    X = np.c_[np.arange(10), np.arange(10)]
+    Y = np.arange(10)
+    data = SplitDataBatch(X, Y)
