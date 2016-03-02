879a960d7a037abd7ed701d7a1a4daea8b12ab5f
Hoang Duong <duongtronghoang@gmail.com>
2016-03-02 10:14:53 -0800

    First Success model of Sequence2Sequence

--- seq2seq.py
+++ seq2seq.py
@@ -18,8 +18,8 @@ class Seq2Seq():
         with tf.Graph().as_default(), tf.Session() as sess:
             n, s, p = data_function.train.X.shape
             _, t, q = data_function.train.Y.shape
-            X_pl    = tf.placeholder(tf.float32, [self.batch_size, s, p])
-            Y_pl    = tf.placeholder(tf.float32, [self.batch_size, t, q])
+            X_pl    = tf.placeholder(tf.float32, [None, s, p])
+            Y_pl    = tf.placeholder(tf.float32, [None, t, q])
             lstm_cell     = rnn_cell.BasicLSTMCell(self.hidden_size)
             cell = tf.nn.rnn_cell.MultiRNNCell([lstm_cell] * self.num_layers)
             _ , enc_state = rnn.rnn(cell, [X_pl[:,i,:] for i in xrange(s)],
@@ -27,10 +27,12 @@ class Seq2Seq():
             outputs, _    = seq2seq.rnn_decoder([Y_pl[:,i,:] for i in 
                                 xrange(t-1)], enc_state, cell)
             concat_outputs = tf.concat(0, outputs)
-            softmax_w = tf.get_variable("softmax_w", [self.hidden_size, q])
-            softmax_b = tf.get_variable("softmax_b", [q])
+            softmax_w = tf.get_variable("softmax_w", [self.hidden_size, q - 1])
+            softmax_b = tf.get_variable("softmax_b", [q - 1])
             logits = tf.matmul(concat_outputs, softmax_w) + softmax_b
-            labels = tf.reshape(Y_pl[:,1:,:], [self.batch_size * (t-1), q])
+            labels = tf.reshape(tf.transpose(Y_pl[:,1:,0]), [-1, 1])
+            ## Be really careful with reshape. They might reshape in the wrong
+            ## direction, i.e. row-wise instead of column-wise
             if self.loss == 'ce':
                 loss = tf.reduce_mean(
                     tf.nn.softmax_cross_entropy_with_logits(logits, labels))
@@ -46,6 +48,7 @@ class Seq2Seq():
             initializer = tf.random_uniform_initializer(-self.init_scale,
                                         self.init_scale)
             tf.initialize_all_variables().run()
+            PrintMessage()
             for i in xrange(self.n_step):
                 batch_xs, batch_ys = data_function.train.next_batch(
                                         self.batch_size)
@@ -53,9 +56,14 @@ class Seq2Seq():
                 _, loss_value = sess.run([train_op, loss],
                         feed_dict = feed_dict)
                 if i % 100 == 0:
-                    ipdb.set_trace()
+                    # ipdb.set_trace()
+                    feed_dict = {X_pl: data_function.validation.X,
+                                 Y_pl: data_function.validation.Y}
+                    loss_valid = sess.run(loss, feed_dict = feed_dict)
+                    score_valid = 1 - loss_valid /\
+                    np.mean(data_function.validation.Y[:,1:,0]**2)
                     PrintMessage(data_function.train.epochs_completed,
-                            loss_value , 0, 1) 
+                            loss_value , loss_valid, score_valid) 
     
                
 if __name__ == '__main_':
@@ -63,6 +71,6 @@ if __name__ == '__main_':
     clf = Seq2Seq(n_step = 100000)
     clf.fit(addition_data)
 if __name__ == '__main__':
-    poly_data = GetPolyData(100000, 10)
-    clf = Seq2Seq(n_step = 100000, num_layers = 3, hidden_size = 256, loss = 'mse')
+    poly_data = GetPolyData(100000, 40)
+    clf = Seq2Seq(n_step = 600000, num_layers = 3, hidden_size = 256, loss = 'mse')
     clf.fit(poly_data)
